80f083c082515fcc185a05965be9d7550861b62a
hyujung <jha0517@hotmail.com>
2023-04-03 14:21:26 +0100

    makefile

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hyunah <hyunah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 16:44:30 by hyunah            #+#    #+#             */
-/*   Updated: 2023/04/02 20:28:06 by hyunah           ###   ########.fr       */
+/*   Updated: 2023/04/03 14:18:29 by hyunah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,14 +42,14 @@ int	main(int ac, char **av)
 	{
 		// add server and set param
 		sm.addServerBlock();
-		sm.servers[i]->setPort(8002);
+		sm.servers[i]->setPort(8001);
 		sm.servers[i]->setHost("127.0.0.1");
 		sm.servers[i]->setMaxClientBodySize(1024);
 		sm.servers[i]->setIndex("index_1.html");
 		sm.servers[i]->setUploadPath("./data/upload");
 
 		std::set<std::string> method;
-		// method.insert("GET");
+		method.insert("GET");
 		method.insert("POST");
 		sm.servers[i]->setAllowedMethod(method);
 
